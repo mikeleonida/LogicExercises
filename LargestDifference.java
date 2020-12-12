627d9457853c689de12ae6a39ac5d391ed50a9bb
unknown <michael.leonida@gmail.com>
2020-12-12 14:02:51 -0500

    LargestDifference code and boundary cases in main

--- LargestDifference.java
+++ LargestDifference.java
@@ -1,3 +1,25 @@
+//**Write a program to read an integer array and find the index of larger number of the 
+//two adjacent numbers with largest difference.
+//Print the index of the larger number.**
+//
+//**This exercise contains a class LargestDifference with the following static method**
+//
+//    public static int findLargestDifference(int[] numbers)
+//        
+//    - Method should take integer array as input and return an integer with the index of 
+//      the larger number with the largest difference.
+//
+//    - Rules: 
+//        - Difference is obtained by subtracting smaller number from larger number
+//        - If the largest difference is same for multiple adjacent numbers, then the 
+//          index of the largest numbers of the larger number should be returned. See 
+//          Sample for example
+//        - If the largest difference is same for multiple adjacent numbers, and the 
+//          larger numbers are also same, then the index of the first larger number should 
+//          be returned
+//    
+//    *Note: If the input array is null or empty, method should return -1*
+
 package exercises;
 
 import java.util.Scanner;
@@ -13,6 +35,12 @@ public class LargestDifference {
 		}
 		String[] userInput = scanner.nextLine().trim().split(" ");
 
+		if (numInts == 0 || numInts == 1 || (userInput.length == 1) || (userInput.length<numInts)) {
+			System.out.println(-1);
+			scanner.close();
+			return;
+		}
+		
 		int[] ldArr = new int[numInts];
 		for (int i=0; i<numInts; i++) {
 			ldArr[i] = Integer.parseInt(userInput[i]);
